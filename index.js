72301ac6196590e29ff3165c10ebd9074949bb98
MevanAhmed <mevanovic@gmail.com>
2022-08-08 11:45:09 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1*num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 9;
+let num4 = 5;
+
+let mod = 9%5;
+
+
+let max = Math.max(20,19,18,1,4,6,8,4,16);
+
+
+
 
 // Write your code here
